e4d11948b53bd79d5f9522916afe74a826c41463
Gowtham K M <116723241+Gowthamk007m@users.noreply.github.com>
2024-01-28 16:10:36 +0530

    Final

--- QR_Code_Generator/qrcodeapp/views.py
+++ QR_Code_Generator/qrcodeapp/views.py
@@ -12,6 +12,13 @@ def qr_gen(request):
         data = request.POST['data']
         img = make(data)
         img_name = 'qr' + str(time.time()) + '.png'
-        img.save(settings.MEDIA_ROOT + '/' + img_name)
+
+        # Use Path.joinpath() to create a valid path
+        img_path = settings.MEDIA_ROOT.joinpath(img_name)
+
+        # Save the image to the path
+        img.save(img_path)
+
         return render(request, 'index.html', {'img_name': img_name})
+    
     return render(request, 'index.html')
